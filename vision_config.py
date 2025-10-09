07f75678c108f5bb033c4d753c9290f13687d7d6
zhough <453854697@qq.com>
2025-10-09 20:24:14 +0800

    modified:   vision_config.py

--- vision_config.py
+++ vision_config.py
@@ -3,7 +3,7 @@ class VisionConfig():
     def __init__(self):
         self.model_name = "google/vit-large-patch16-224"
         self.num_layers = 3
-        self.v_hidden_size = 768
+        self.v_hidden_size = 1024
         self.num_heads = 8
         self.fusion_layers = [0,1,2,10,11,12,13,14,15,20,21,22]
         #self.fusion_layers = [i for i in range(0,28)]
